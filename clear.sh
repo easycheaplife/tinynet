46b0779e1822d9a4942913cbf18ea1a8e528c58e
King Lee <lijin@chukong-inc.com>
2014-11-25 16:34:00 +0800

    update  cmakelists

--- clear.sh
+++ clear.sh
@@ -4,4 +4,13 @@ rm -rf CMakeFiles
 rm -f cmake_install.cmake
 rm -f CMakeCache.txt
 rm -f Makefile
+rm -rf bin
+rm -rf lib
+rm -rf cli_test/CMakeFiles
+rm -f cli_test/cmake_install.cmake
+rm -f cli_test/Makefile
+rm -rf srv_test/CMakeFiles
+rm -f srv_test/cmake_install.cmake
+rm -f srv_test/Makefile
+
 echo 'end clear'
